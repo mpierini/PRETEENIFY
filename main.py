1bcf3c7722dc3d15a27b537e0c4cc68f80f73ee6
Michele <pierini.michele@gmail.com>
2013-07-21 18:54:09 -0700

    added postgresql db connection

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-import os
+import os, psycopg2, urlparse
+
+#LOCAL DB
+#import os, pymongo
+#from pymongo import Connection
+
 from bottle import route, run, template, get, post, request, static_file, error
 
+def connect():
+    urlparse.uses_netloc.append("postgres")
+    url = urlparse.urlparse(os.environ["DATABASE_URL"])
+    conn = psycopg2.connect(
+        database = url.path[1:],
+	user = url.username,
+	password = url.password,
+	host = url.hostname,
+	port = url.port
+    )
+
+def save_string(str):
+    connection = connect()
+    cur = connection.cursor()
+    query = "INSERT INTO preteenify VALUES (" + str + ");"
+    cur.execute(query)'''
+
+def save_string(str):
+    connection = Connection()
+    db = connection.preteenify
+    collection = db.strings
+    entry = {'text' : str}
+    collection.insert(entry)
 
 @route('/')
 def serve_index():
@@ -10,13 +38,14 @@ def serve_index():
 def serve_translation():
     word_string = request.forms.get('word_string')
     new_string = translate(word_string)
+    save_string(new_string)
     return '''<link rel=stylesheet type=text/css href="static/style.css">
 	      <body>
     	        <h1 align = "center">
       		  <a href="/">PRETEENIFY</a>
     		</h1>
     		<div class="words">
-		((~* '''+ new_string +''' *~))</div>
+		'''+ new_string +'''</div>
     		<div class="dolphin" align="top">
       		  <img src="static/dolphin.gif">
     		</div>
@@ -46,9 +75,12 @@ def translate(word_string):
 	'love' : 'luv',
 	'haha' : 'lol',
 	'oh my god' : 'omg',
-	'night' : 'nite',
+	'ight' : 'ite',
 	'girl' : 'gurl',
 	'and' : '&',
+	'because' : 'cuz',
+	'forever' : '4ever',
+	'what' : 'wut',
     } 
 
     for key in vocab_dict:
@@ -67,7 +99,7 @@ def translate(word_string):
 	        each = each.replace(letter, letter.upper())
         words[index] = each
 	index+=1
-    return ' '.join(words)
+    return '((~* ' + ' '.join(words) + ' *~))'
 
 
 @route('/static/<filename>')
