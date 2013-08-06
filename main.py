1498734b7bfe6ee8a7ef7f6f65b4de92f85e6a9f
Michele <pierini.michele@gmail.com>
2013-08-05 23:34:49 -0700

    added twitter feed widget

--- main.py
+++ main.py
@@ -61,13 +61,14 @@ def serve_translation():
       		  <img src="static/palm.gif">
     		</div>
     		<div class="bieber" align="center">
-      		  <img src="static/leftJB.gif">
+                  <a class="twitter-timeline" width="300" height="450" href="https://twitter.com/PRETEENIFY"  data-widget-id="364628094939717632">Tweets by @PRETEENIFY</a>
+                  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
     		</div>
     		<div class="pizza" align="left">
       		  <img src="static/adventurePIZZA.gif">
     		</div>
     		<div class="cat" align="right">
-      		  <img src="static/spacecat.gif">
+                  <img src="static/spacecat.gif">
     		</div>
   	      </body>'''
     
